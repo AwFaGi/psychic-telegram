cfe540ed9e4cedea589cd957e2c8adacc8090f0d
Stepan K <my.sky12@yandex.ru>
2022-04-27 15:29:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Random bot for Telegram
+Link: [@heh_random_bot](https://t.me/heh_random_bot)
+
+It can work with text and images. Just send message with necessary content and bot will reply with selected choice
+# Available commands:
+- /dice
+- /range
+# Requirements
+- Environment variable `"TG_BOT_TOKEN"` or `.env` file with it
+- Packages from `requirements.txt`
---
+++ main.py
@@ -0,0 +1,79 @@
+import time
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import logging
+from typing import List
+from random import choice, randrange
+import os
+from aiogram_media_group import MediaGroupFilter, media_group_handler
+from dotenv import load_dotenv
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+
+TOKEN = os.environ.get("TG_BOT_TOKEN", None)
+if not TOKEN:
+    logging.log(logging.ERROR, 'Set "TG_BOT_TOKEN"')
+    raise SystemExit
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+async def help_func(message: types.Message):
+    s = "Для выбора текстовых значений, напиши их по одному в строке.\nДля выбора фотографий, отправь их в одном сообщении\n" + \
+        "\n".join([f"<i>/{i}:</i> {bot_commands[i][1]}" for i in bot_commands.keys()])
+    await message.answer(s, parse_mode="HTML")
+
+async def dice_func(message: types.Message): await message.reply_dice()
+
+async def range_func(message: types.Message):
+    try:
+        s = list(map(int, message.get_args().split()))
+        if len(s) == 0:
+            await message.reply( randrange(1, 101) )
+        else:
+            await message.reply( randrange(*s) )
+    except Exception as e: 
+        await message.reply("Произошла ошибка!")
+
+bot_commands = {
+    "help": (help_func, "Пишет данную справку."),
+    "dice": (dice_func, "Бросает кубик."),
+    "range": (range_func, "Принимает до 3 целых чисел: \
+        <code>\nконец\nначало конец\nначало конец шаг\n</code>Возвращает целое число из указанного промежутка.\
+        По умолчанию: <i><code>/range 1 100</code></i>")
+}
+
+@dp.message_handler(commands=bot_commands.keys())
+async def process(message: types.Message): await bot_commands[message.get_command()[1:]][0](message)
+
+@dp.message_handler(commands="start")
+async def start_func(message: types.Message): await message.answer("Привет! Я помогу тебе сделать выбор в трудной ситуации. Попробуй написать /help")
+
+AVAILABLE_GROUP_COMMANDS = ["/random", "/выборка", "/"]
+@dp.message_handler(content_types=types.ContentTypes.TEXT)
+async def master(message: types.Message):
+    if message.chat.type == "private":
+        await message.reply( choice( message.text.splitlines() ) )
+    elif message.chat.type == "group":
+        cash = message.text.splitlines()
+        if cash [0] in AVAILABLE_GROUP_COMMANDS and len(cash) > 1:
+            await message.reply( choice (cash [1:]))
+        elif cash [0] in AVAILABLE_GROUP_COMMANDS:
+            await message.reply( "Мне нужны варианты!" )
+
+@dp.message_handler(MediaGroupFilter(), content_types=types.ContentType.PHOTO)
+@media_group_handler
+async def album_handler(messages: List[types.Message]):
+    msg = choice(messages)
+    await msg.reply_photo(msg.photo[-1].file_id)
+
+@dp.message_handler(content_types=types.ContentType.PHOTO)
+async def album_handler(message: types.Message): await message.reply_photo(message.photo[-1].file_id)
+
+@dp.message_handler(content_types=types.ContentType.ANY)
+async def master(message: types.Message): await message.reply("Извини, я пока не могу с таким работать. Попробуй /help")
+
+if __name__ == "__main__":
+    while True:
+        try: executor.start_polling(dp, skip_updates=True)
+        except: time.sleep(5)
